a9a7fb7053ffd739ef32c0ff0dbd16fed57c1737
replicantStarr <134128829+replicantStarr@users.noreply.github.com>
2026-08-30 21:30:50 +1000

    Basic flask backend

--- learning-resource-manager/backend/main.py
+++ learning-resource-manager/backend/main.py
@@ -1,5 +1,26 @@
+import os
+from flask import flask
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, f"../database/{os.getenv('DATABASE_NAME')}"
+    )
+    
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    @app.route("/test")
+    def test():
+        return "testing123"
+
+    return app
+
+
 def main():
-    print("no code")
+   app = create_app()
+   input()
 
 if __name__ == "__main__":
     main()
