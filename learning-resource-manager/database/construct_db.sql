5a1e0a522b433a5802bf14a08bff289cdea651f8
replicantStarr <134128829+replicantStarr@users.noreply.github.com>
2026-08-30 19:54:35 +1000

    Quick changes

---
+++ learning-resource-manager/database/construct_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE
+
+
+/*
+We need the following tables
+1. Resource
+	- ID
+	- Name
+	- Location
+	- Author
+	- Date of Publishing
+	- Size
+	- Subject
+
+2. 
+*/
--- learning-resource-manager/database/database_init.py
+++ learning-resource-manager/database/database_init.py
@@ -1,5 +1,23 @@
+import sqlite3
+import os
+
+def create_database() -> string:
+    db_name = os.getenv("DATABASE_NAME")
+    db_con = sqlite3.connect(db_name)
+
+    return db_con
+
+def construct_database(db_connection: string) -> None:
+    with open('construct_db.sql', 'r') as script:
+        cursor = db_connection.cursor()
+        cursor.executescript(script)
+        cursor.commit()
+        cursor.close()
+
 def main():
-    pass
+    db = create_database()
+    construct_database(db)
+    db.close()
 
 if __name__ == "__main__":
     main()
