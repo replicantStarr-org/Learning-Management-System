e315bc84584ce2d1f0fdfbd789adfbf91cbbcd1b
replicantStarr <134128829+replicantStarr@users.noreply.github.com>
2026-08-31 14:51:25 +1000

    Fixed backend container, basic code is working

--- learning-resource-manager/backend/main.py
+++ learning-resource-manager/backend/main.py
@@ -1,12 +1,12 @@
 import os
-from flask import flask
+from flask import Flask
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, f"../database/{os.getenv('DATABASE_NAME')}"
+        DATABASE=os.path.join(app.instance_path, f"../database/{os.getenv('DATABASE_NAME')}")
     )
     
     os.makedirs(app.instance_path, exist_ok=True)
@@ -20,7 +20,6 @@ def create_app():
 
 def main():
    app = create_app()
-   input()
 
 if __name__ == "__main__":
     main()
