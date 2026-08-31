b5de4b5b40214762a250babb37ae447c288a6c48
replicantStarr <134128829+replicantStarr@users.noreply.github.com>
2026-08-31 18:40:41 +1000

    Additional env file to docker container

--- learning-resource-manager/backend/controllers/resources_controller.py
+++ learning-resource-manager/backend/controllers/resources_controller.py
@@ -1,7 +1,14 @@
 from flask import Blueprint
+import os
+import sqlite3
 
 resources_bp = Blueprint('resources', __name__)
 
 @resources_bp.route('/resources')
 def get_resources():
-    return {}
+    db_name = os.getenv("DATABASE_NAME")
+    with sqlite.connect(db_name) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM l_resource")
+
+        return cursor.fetchall()
