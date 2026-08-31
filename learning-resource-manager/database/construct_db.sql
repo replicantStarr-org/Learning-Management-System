b5db818b24eed7a7713cfa9b07dd9eb03cbe9d92
replicantStarr <134128829+replicantStarr@users.noreply.github.com>
2026-09-01 03:49:42 +1000

    Container for all 3 parts are working

--- learning-resource-manager/database/construct_db.sql
+++ learning-resource-manager/database/construct_db.sql
@@ -1,12 +1,9 @@
 CREATE TABLE l_resource ( 
 	l_resource_id INTEGER PRIMARY KEY,
 	author VARCHAR(50),
-	medium_id INTEGER NOT NULL,
+	medium VARCHAR(32) NOT NULL,
 	title VARCHAR(50) NOT NULL,
-	description TEXT,
-
-	CONSTRAINT FK_medium
-	FOREIGN KEY (medium) REFERENCES medium(medium_id)
+	description TEXT
 );
 
 CREATE TABLE subjects(
@@ -14,28 +11,23 @@ CREATE TABLE subjects(
 	name VARCHAR(32)
 );
 
-CREATE TABLE medium (
-	medium_id INTEGER PRIMARY KEY,
-	name VARCHAR(16)
-);
-
 CREATE TABLE l_resource_subject (
 	l_resource_id INTEGER NOT NULL,
 	subject_id INTEGER NOT NULL,
 
-	CONSTRAINT FK_l_resource_id
-	FOREIGN KEY (l_resource_id) REFERENCES l_resource(l_resource_id)
-
-	CONSTRAINT FK_subject_id
-	FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
+	PRIMARY KEY (l_resource_id, subject_id),
+	CONSTRAINT FK_l_resource_id FOREIGN KEY (l_resource_id) REFERENCES l_resource(l_resource_id),
+	CONSTRAINT FK_subject_id FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
 );
 
 CREATE TABLE l_resource_highlight(
-	l_resource_highlight_id INTEGER NOT NULL,
+	l_resource_highlight_id INTEGER PRIMARY KEY,
+	l_resource_id INTEGER NOT NULL,
 	user_id INTEGER NOT NULL,
 	name VARCHAR(32),
 	begin_row INTEGER NOT NULL,
 	begin_char INTEGER NOT NULL,
 	end_row INTEGER NOT NULL,
 	end_char INTEGER NOT NULL,
+	CONSTRAINT FK_highlight_resource FOREIGN KEY (l_resource_id) REFERENCES l_resource(l_resource_id)
 );
--- learning-resource-manager/database/database_init.py
+++ learning-resource-manager/database/database_init.py
@@ -1,21 +1,31 @@
 import sqlite3
 import os
+from pathlib import Path
 
-def create_database() -> string:
+def create_database() -> str:
     db_name = os.getenv("DATABASE_NAME")
     db_path = os.getenv("DATABASE_PATH")
     db_con = sqlite3.connect(db_path + "/" + db_name)
 
     return db_con
 
-def construct_database(db_connection: string) -> None:
+def construct_database(db_connection: str) -> None:
     with open('construct_db.sql', 'r') as script:
         cursor = db_connection.cursor()
-        cursor.executescript(script)
-        cursor.commit()
+        cursor.executescript(script.read())
+        db_connection.commit()
         cursor.close()
 
+def database_exists():
+    db_name = os.getenv("DATABASE_NAME")
+    db_path = os.getenv("DATABASE_PATH")
+    path = Path(db_path + "/" + db_name)
+    return path.exists()
+
 def main():
+    if database_exists():
+        return
+
     db = create_database()
     construct_database(db)
     db.close()
